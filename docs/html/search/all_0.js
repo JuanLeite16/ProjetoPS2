2a7369b7866c90139f11177fcce4022c7a8c0deb
JuanLeite16 <a36642@alunos.ipca.pt>
2026-01-16 11:02:22 +0000

    Documentação em doxygen

---
+++ docs/html/search/all_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['análise_20de_20arquivos_20ps2_0',['Análise de arquivos PS2',['../md_README.html#autotoc_md0',1,'']]],
+  ['arquivos_20ps2_1',['Análise de arquivos PS2',['../md_README.html#autotoc_md0',1,'']]]
+];
---
+++ docs/html/search/searchdata.js
@@ -0,0 +1,18 @@
+var indexSectionsWithContent =
+{
+  0: "acdeopr",
+  1: "r"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "Tudo",
+  1: "Páginas"
+};
+
