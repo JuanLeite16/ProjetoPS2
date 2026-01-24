3bee9b5f551d00b5a84d8b6ecec1023fdf7a3c7a
JuanLeite16 <a36642@alunos.ipca.pt>
2026-01-24 23:46:21 +0000

    Ficheiros C

---
+++ src/C/main.c
@@ -0,0 +1,14 @@
+#include <string.h>
+#include <stdio.h>
+#include <strings.h>
+typedef struct {
+    int id_cliente;
+    char nome[100];
+    char nif[10];
+    char nib[22];
+} Cliente;
+
+int main(){
+    Cliente Clientes[100];
+    return 0;
+}
